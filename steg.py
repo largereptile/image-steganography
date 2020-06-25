bd288e5396b2a84f17761e638361a2a147045b94
largereptile <harry@yark.org.uk>
2020-06-25 03:22:33 +0100

    added some comments

--- steg.py
+++ steg.py
@@ -9,11 +9,10 @@ from werkzeug.utils import secure_filename
 from image_editor import ImageEditor
 
 app = Flask(__name__)
-CORS(app)
+CORS(app)  # oof ouch cant request media from myself without this
 
-UPLOAD_FOLDER = 'uploads/'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 INVALID = {'url': "invalid"}
 
 
@@ -28,14 +27,15 @@ def allowed_file(file_ext):
     return file_ext.strip(".") in ALLOWED_EXTENSIONS
 
 
-def encode_from_temp(old_filename, png_filename, message):
+def encode_to_file(old_filename, png_filename, message):
+    """Use ImageEditor to write message to image and save locally"""
     img = ImageEditor(old_filename)
     img.encode(message)
     os.remove(old_filename)
     img.save_changes(filename=png_filename)
 
 
-def decode_from_temp(filename):
+def decode_from_file(filename):
     img = ImageEditor(filename)
     msg = img.extract_message()
     return msg
@@ -48,26 +48,29 @@ def uploaded_file(filename):
 
 @app.route('/steganography/encode', methods=['POST'])
 def encode():
-    if request.method == 'POST':
-        if 'file' not in request.files:
+    if request.method == 'POST':  # no get requests here kiddo
+        if 'file' not in request.files:  # ensures file is given
             flash('No file part')
             return jsonify(INVALID)
-        message = request.form['message']
+        message = request.form['message']  # TODO check message exists
         file = request.files['file']
-        if file.filename == '':
+        if file.filename == '':  # makes sure filename exists
             flash('No selected file')
             return jsonify(INVALID)
+
         file_ext = os.path.splitext(file.filename)[1]
         random_name = random_string(8)
-        old_filename = f"{random_name}{file_ext}"
+        old_filename = f"{random_name}{file_ext}"  # preserving file extension, might not be necessary
         old_filename = os.path.join(app.config['UPLOAD_FOLDER'], old_filename)
         png_filename = f"{random_name}.png"
         png_filename = os.path.join(app.config['UPLOAD_FOLDER'], png_filename)
+
         if file and allowed_file(file_ext):
             file.save(old_filename)
-            encode_from_temp(old_filename, png_filename, message)
+            encode_to_file(old_filename, png_filename, message)
             filename = f"{random_name}.png"
             return jsonify({'url': f"https://harru.club:5000{url_for('uploaded_file', filename=filename)}"})
+
     return jsonify(INVALID)
 
 
@@ -81,17 +84,21 @@ def decode():
         if file.filename == '':
             flash('No selected file')
             return jsonify(INVALID)
+
         file_ext = os.path.splitext(file.filename)[1]
         filename = secure_filename(f"{random_string(8)}{file_ext}")
         filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
         if file and allowed_file(file_ext):
             file.save(filename)
-            message = decode_from_temp(filename)
+            message = decode_from_file(filename)
             return jsonify({'message': message})
+
         return jsonify(INVALID)
 
 
 if __name__ == "__main__":
+    # hmm maybe i should have removed these
     context = ("/etc/letsencrypt/live/harru.club/fullchain.pem", "/etc/letsencrypt/live/harru.club/privkey.pem")
     app.run(host='0.0.0.0', port=5000, ssl_context=context)
 
